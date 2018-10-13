4170d313ba4329e228c947f3ed89aaea2bbcf344
dixx <jenzdixx@googlemail.com>
2018-10-13 22:55:52 +0200

    Add SD card functionality, WIP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,48 @@
 #include "Arduino.h"
+#include "Storage.h"
 
 void setup() {
+    Serial.begin(115200);
+    Storage::init();
+
+    Serial.print("Creating example.txt...");
+    File myFile = SD.open("example.txt", FILE_WRITE);
+    if (myFile) {
+        myFile.println("This");
+        myFile.println("Is");
+        myFile.println("A (äß)");
+        myFile.println("Test.");
+        myFile.close();
+        if (!SD.exists("example.txt")) {
+            Serial.println("creation failed!");
+            return;
+        }
+    } else {
+        Serial.println("opening file failed!");
+        return;
+    }
+    Serial.println("done.");
+
+    Serial.println("Reading example.txt...\n---");
+    myFile = SD.open("example.txt");
+    if (myFile) {
+        while (myFile.available()) {
+            Serial.write(myFile.read());
+        }
+        myFile.close();
+    } else {
+        Serial.println("opening file failed!");
+        return;
+    }
+    Serial.println("---\ndone.");
+
+    Serial.print("Removing example.txt...");
+    SD.remove("example.txt");
+    if (SD.exists("example.txt")) {
+        Serial.println("failed!");
+        return;
+    }
+    Serial.println("done.");
 }
 
 void loop() {
