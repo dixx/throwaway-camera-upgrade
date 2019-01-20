1c56e6f104e73a2f4bd18da6e2e5af36909e65a9
dixx <jenzdixx@googlemail.com>
2019-01-20 11:41:18 +0100

    Add Storage class

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,16 @@
 #include "Arduino.h"
 #include "Storage.h"
 
+Storage storage;
+
 void setup() {
     Serial.begin(115200);
-    Storage::init();
-
-    Serial.print("Creating example.txt...");
-    File myFile = SD.open("example.txt", FILE_WRITE);
-    if (myFile) {
-        myFile.println("This");
-        myFile.println("Is");
-        myFile.println("A (äß)");
-        myFile.println("Test.");
-        myFile.close();
-        if (!SD.exists("example.txt")) {
-            Serial.println("creation failed!");
-            return;
-        }
-    } else {
-        Serial.println("opening file failed!");
-        return;
+    storage.init();
+    if (storage.is_ready()) {
+        storage.write_text("example.txt", "This is a test.\näöüß\n\tè^°");
+        storage.read_text("example.txt");
+        storage.remove("example.txt");
     }
-    Serial.println("done.");
-
-    Serial.println("Reading example.txt...\n---");
-    myFile = SD.open("example.txt");
-    if (myFile) {
-        while (myFile.available()) {
-            Serial.write(myFile.read());
-        }
-        myFile.close();
-    } else {
-        Serial.println("opening file failed!");
-        return;
-    }
-    Serial.println("---\ndone.");
-
-    Serial.print("Removing example.txt...");
-    SD.remove("example.txt");
-    if (SD.exists("example.txt")) {
-        Serial.println("failed!");
-        return;
-    }
-    Serial.println("done.");
 }
 
 void loop() {
