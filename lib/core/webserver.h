b52422a214c85d032a516e39a666ca8f1011c18a
dixx <jenzdixx@googlemail.com>
2019-01-26 19:13:51 +0100

    Add different pages

--- lib/core/webserver.h
+++ lib/core/webserver.h
@@ -14,20 +14,6 @@ namespace webserver {
 
     ESP8266WebServer server(80);
 
-    bool sendFileContent(String path);
-
-    void init() {
-        if (ready) return;
-        serial::init();
-        spiffs::init();
-        wifi::init();
-        server.onNotFound([]() {
-            if (!sendFileContent(server.uri()))
-                server.send(404, "text/plain", "404: Not Found");
-        });
-        ready = true;
-    }
-
     String getContentType(const String& filename) { // convert the file extension to the MIME type
         if (filename.endsWith(".html")) return "text/html";
         else if (filename.endsWith(".css")) return "text/css";
@@ -47,6 +33,66 @@ namespace webserver {
         Serial.print("\tFile Not Found: ");
         Serial.println(path);
         return false;
+    }
+
+    void handlePics() {
+        sendFileContent("/");
+    }
+
+    void handleSubmit() {
+      // String LEDvalue;
+      //
+      // LEDvalue = server.arg("PINS");
+      // if (LEDvalue == "1") {
+      //   writeLED(true);
+      //   server.send(200, "text/html", INDEX_HTML);
+      // }
+      // else if (LEDvalue == "0") {
+      //   writeLED(false);
+      //   server.send(200, "text/html", INDEX_HTML);
+      // }
+      // else {
+      //   returnFail("Bad LED value");
+      // }
+      sendFileContent("/");
+    }
+
+    void handlePrefs() {
+        if (server.hasArg("PIN01"))
+          handleSubmit();
+        sendFileContent("/prefs.html");
+    }
+
+    void handleStats() {
+        sendFileContent("/stats.html");
+    }
+
+    void handleNotFound() {
+        if (!sendFileContent(server.uri())) {
+            String message = "File Not Found\n\nURI: ";
+            message += server.uri();
+            message += "\nMethod: ";
+            message += (server.method() == HTTP_GET) ? "GET" : "POST";
+            message += "\nArguments: ";
+            message += server.args();
+            message += "\n";
+            for (uint8_t i = 0; i < server.args(); i++) {
+              message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+            }
+            server.send(404, "text/plain", message);
+        }
+    }
+
+    void init() {
+        if (ready) return;
+        serial::init();
+        spiffs::init();
+        wifi::init();
+        server.on("/", handlePics);
+        server.on("/prefs", handlePrefs);
+        server.on("/stats", handleStats);
+        server.onNotFound(handleNotFound);
+        ready = true;
     }
 }
 
